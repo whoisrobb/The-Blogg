298b5776c4b9f67163f1676e221d049ab54c08f6
Robbie Muchiri <developedbyrobbie@gmail.com>
2023-08-24 23:48:32 +0300

    navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,18 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost'
+import Header from './components/Header'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/create' element={<CreatePost />} />
-    </Routes>
+    <>
+      <Header />
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/create' element={<CreatePost />} />
+      </Routes>
+    </>
   )
 }
 

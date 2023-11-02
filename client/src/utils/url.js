4957c219dc43e75770b867216525c071076f7b6f
Robbie Muchiri <developedbyrobbie@gmail.com>
2023-11-02 23:22:50 +0300

    deployment settings

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://localhost:3000'
-// export const apiUrl = 'https://the-blogg-api.vercel.app'
+// export const apiUrl = 'http://localhost:3000'
+export const apiUrl = 'https://the-blogg.onrender.com'
